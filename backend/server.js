49d6eb538df25d6ca5ef54cc0ef6bef5ba4ef537
Vaishnavi Singh <vaishnavi.singh@sigmoidanalytics.com>
2026-04-07 14:30:37 +0530

    frontend

--- backend/server.js
+++ backend/server.js
@@ -38,9 +38,9 @@ app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "frontend/build")));
+app.use(express.static(path.join(__dirname, "./frontend/build")));
 app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "frontend/build/index.html"))
+  res.sendFile(path.join(__dirname, "./frontend/build/index.html"))
 );
 
 app.use((err, req, res, next) => {
